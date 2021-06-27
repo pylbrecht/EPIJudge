7f1b25fd48b265a7a1726b75a969c1c1b64b42f6
pylbrecht <pylbrecht@gmail.com>
2021-06-27 21:09:33 +0200

    Implement int_as_array_increment

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,11 +1,28 @@
 from typing import List
+from collections import deque
 
 from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1
+    carry = A[-1] // 10
+
+    if not carry:
+        return A
+
+    result = deque()
+    result.appendleft(A[-1] % 10)
+
+    for digit in reversed(A[:-1]):
+        digit += carry
+        carry = digit // 10
+        result.appendleft(digit % 10)
+
+    if carry:
+        result.appendleft(1)
+
+    return list(result)
 
 
 if __name__ == '__main__':
