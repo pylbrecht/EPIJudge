a3795e60b0d390ecf1b48ea4fb631425be2845e2
pylbrecht <pylbrecht@gmail.com>
2021-07-05 10:36:21 +0200

    Implement evaluate_rpn

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,28 @@
+import operator
+from typing import List
 from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack: List[int] = []
+    operators = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.floordiv,
+    }
+    tokens = expression.split(',')
+    for token in tokens:
+        try:
+            number = int(token)
+        except ValueError:
+            # found operator
+            right, left = stack.pop(), stack.pop()
+            number = operators[token](left, right)
+
+        stack.append(number)
+
+    return stack.pop()
 
 
 if __name__ == '__main__':
