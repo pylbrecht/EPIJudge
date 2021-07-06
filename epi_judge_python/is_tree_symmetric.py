3132584f0cc21d0d6d65ae507acca2358383de72
pylbrecht <pylbrecht@gmail.com>
2021-07-06 16:34:54 +0200

    Implement is_tree_symmetric

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,12 +2,28 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+def _is_symmetric(subtree0: BinaryTreeNode, subtree1: BinaryTreeNode) -> bool:
+    if not subtree0 and not subtree1:
+        # leaf
+        return True
+
+    if subtree0 and subtree1:
+        subtrees_have_equal_data = subtree0.data == subtree1.data
+        subtrees_are_symmetric = _is_symmetric(
+            subtree0.left, subtree1.right
+        ) and _is_symmetric(subtree0.right, subtree1.left)
+        return subtrees_have_equal_data and subtrees_are_symmetric
+
+    return False
+
+
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    return not tree or _is_symmetric(tree.left, tree.right)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('is_tree_symmetric.py',
-                                       'is_tree_symmetric.tsv', is_symmetric))
+        generic_test.generic_test_main(
+            "is_tree_symmetric.py", "is_tree_symmetric.tsv", is_symmetric
+        )
+    )
